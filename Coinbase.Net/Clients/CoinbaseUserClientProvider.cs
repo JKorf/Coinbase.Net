10f29c0b982911ae7ce77b8f946dcea9d8e026d3
Jkorf <jankorf91@gmail.com>
2025-08-22 14:53:22 +0200

    Added ClearUserClients method to user client provider

--- Coinbase.Net/Clients/CoinbaseUserClientProvider.cs
+++ Coinbase.Net/Clients/CoinbaseUserClientProvider.cs
@@ -54,6 +54,13 @@ namespace Coinbase.Net.Clients
         }
 
         /// <inheritdoc />
+        public void ClearUserClients(string userIdentifier)
+        {
+            _restClients.TryRemove(userIdentifier, out _);
+            _socketClients.TryRemove(userIdentifier, out _);
+        }
+
+        /// <inheritdoc />
         public ICoinbaseRestClient GetRestClient(string userIdentifier, ApiCredentials? credentials = null, CoinbaseEnvironment? environment = null)
         {
             if (!_restClients.TryGetValue(userIdentifier, out var client))

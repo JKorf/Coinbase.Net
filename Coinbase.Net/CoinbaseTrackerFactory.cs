21171c5f74da77d30865a4b65abb9a0e9c07bd19
Jkorf <jankorf91@gmail.com>
2026-02-03 16:40:29 +0100

    wip

--- Coinbase.Net/CoinbaseTrackerFactory.cs
+++ Coinbase.Net/CoinbaseTrackerFactory.cs
@@ -1,11 +1,16 @@
 ﻿using Coinbase.Net.Clients;
 using Coinbase.Net.Interfaces;
 using Coinbase.Net.Interfaces.Clients;
+using CryptoExchange.Net.Authentication;
 using CryptoExchange.Net.SharedApis;
 using CryptoExchange.Net.Trackers.Klines;
 using CryptoExchange.Net.Trackers.Trades;
+using CryptoExchange.Net.Trackers.UserData;
+using CryptoExchange.Net.Trackers.UserData.Interfaces;
+using CryptoExchange.Net.Trackers.UserData.Objects;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using System;
 
 namespace Coinbase.Net
@@ -73,6 +78,68 @@ namespace Coinbase.Net
                 symbol,
                 limit,
                 period
+                );
+        }
+
+        /// <inheritdoc />
+        public IUserSpotDataTracker CreateUserSpotDataTracker(SpotUserDataTrackerConfig config)
+        {
+#warning best way to have a default interval since we don't have a subscription available?
+            if (config.BalancesConfig.PollIntervalConnected == TimeSpan.Zero)
+                config.BalancesConfig.PollIntervalConnected = TimeSpan.FromSeconds(10);
+
+            var restClient = _serviceProvider?.GetRequiredService<ICoinbaseRestClient>() ?? new CoinbaseRestClient();
+            var socketClient = _serviceProvider?.GetRequiredService<ICoinbaseSocketClient>() ?? new CoinbaseSocketClient();
+            return new CoinbaseUserSpotDataTracker(
+                _serviceProvider?.GetRequiredService<ILogger<CoinbaseUserSpotDataTracker>>() ?? new NullLogger<CoinbaseUserSpotDataTracker>(),
+                restClient,
+                socketClient,
+                null,
+                config
+                );
+        }
+
+        /// <inheritdoc />
+        public IUserSpotDataTracker CreateUserSpotDataTracker(string userIdentifier, SpotUserDataTrackerConfig config, ApiCredentials credentials, CoinbaseEnvironment? environment = null)
+        {
+            var clientProvider = _serviceProvider?.GetRequiredService<ICoinbaseUserClientProvider>() ?? new CoinbaseUserClientProvider();
+            var restClient = clientProvider.GetRestClient(userIdentifier, credentials, environment);
+            var socketClient = clientProvider.GetSocketClient(userIdentifier, credentials, environment);
+            return new CoinbaseUserSpotDataTracker(
+                _serviceProvider?.GetRequiredService<ILogger<CoinbaseUserSpotDataTracker>>() ?? new NullLogger<CoinbaseUserSpotDataTracker>(),
+                restClient,
+                socketClient,
+                userIdentifier,
+                config
+                );
+        }
+
+        /// <inheritdoc />
+        public IUserFuturesDataTracker CreateUserFuturesDataTracker(FuturesUserDataTrackerConfig config)
+        {
+            var restClient = _serviceProvider?.GetRequiredService<ICoinbaseRestClient>() ?? new CoinbaseRestClient();
+            var socketClient = _serviceProvider?.GetRequiredService<ICoinbaseSocketClient>() ?? new CoinbaseSocketClient();
+            return new CoinbaseUserFuturesDataTracker(
+                _serviceProvider?.GetRequiredService<ILogger<CoinbaseUserFuturesDataTracker>>() ?? new NullLogger<CoinbaseUserFuturesDataTracker>(),
+                restClient,
+                socketClient,
+                null,
+                config
+                );
+        }
+
+        /// <inheritdoc />
+        public IUserFuturesDataTracker CreateUserFuturesDataTracker(string userIdentifier, FuturesUserDataTrackerConfig config, ApiCredentials credentials, CoinbaseEnvironment? environment = null)
+        {
+            var clientProvider = _serviceProvider?.GetRequiredService<ICoinbaseUserClientProvider>() ?? new CoinbaseUserClientProvider();
+            var restClient = clientProvider.GetRestClient(userIdentifier, credentials, environment);
+            var socketClient = clientProvider.GetSocketClient(userIdentifier, credentials, environment);
+            return new CoinbaseUserFuturesDataTracker(
+                _serviceProvider?.GetRequiredService<ILogger<CoinbaseUserFuturesDataTracker>>() ?? new NullLogger<CoinbaseUserFuturesDataTracker>(),
+                restClient,
+                socketClient,
+                userIdentifier,
+                config
                 );
         }
     }
